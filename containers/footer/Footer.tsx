df0d55b8f6fd8e5352bf5b4d9d7019065747c415
Hamid Nawaz <hamid.nawaz28@yahoo.com>
2021-10-13 15:13:35 +0500

    Rouglhy added affiliations

--- containers/footer/Footer.tsx
+++ containers/footer/Footer.tsx
@@ -1,12 +1,6 @@
 import styles from "../../styles/Home.module.css";
 import { Grid, Typography, Link } from "@mui/material";
-import {
-  Facebook,
-  YouTube,
-  Instagram,
-  Telegram,
-  WhatsApp,
-} from "@mui/icons-material";
+import { Facebook, YouTube, Instagram, WhatsApp } from "@mui/icons-material";
 import { makeStyles } from "@mui/styles";
 
 const useStyles = makeStyles((theme: any) => {
@@ -49,11 +43,11 @@ const useStyles = makeStyles((theme: any) => {
 });
 
 const breakpoints = {
-  xs: 6,
-  sm: 6,
-  md: 3,
-  lg: 3,
-  xl: 3,
+  xs: 12,
+  sm: 12,
+  md: 4,
+  lg: 4,
+  xl: 4,
 };
 
 const links: any = {
@@ -73,15 +67,19 @@ const siteLinks: any = [
     href: "/contact-us",
   },
 ];
+
 const email = "info.grpsytours@gmail.pk";
 const waLink = "https://wa.me/message/BQNERPAFPKHYE1";
+const qrUrl = "/others/qr-code.png";
+const qrAlt = "Gypsy Traces QR";
 const address = [
   "Flat 6D/1, Askari Towers-II",
   "Sector D, DHA-II, Islamabad Pakistan",
 ];
 
 export default function Footer() {
-  const { grid } = useStyles();
+  const { grid, footer, copyright, footerContainer, link } = useStyles();
+
   const GridWrap = ({ children }: any) => (
     <Grid {...breakpoints} item className={grid}>
       {children}
@@ -95,52 +93,79 @@ export default function Footer() {
     youtube: <YouTube />,
   };
 
-  const { footer, copyright, footerContainer, link } = useStyles();
   return (
     <footer className={footer}>
-      <Grid container className={footerContainer}>
-        <GridWrap>
-          <h3>Location</h3>
-          {address.map((el, id) => (
-            <p key={id}>{el}</p>
-          ))}
-        </GridWrap>
-        <GridWrap>
-          <h3>Social Media</h3>
-          {Object.keys(links).map((item: any, id: number) => {
-            const url = links[item];
-            return (
-              <Link href={url} target="_blank" className={link} key={id}>
-                {icons[item]}
-              </Link>
-            );
-          })}
-        </GridWrap>
-        <GridWrap>
-          <h3>Links</h3>
-          {siteLinks.map((item: any, id: number) => {
-            return (
-              <p key={id}>
-                <Link
-                  href={item.href}
-                  target="_blank"
-                  className={link}
-                  underline="none"
-                >
-                  {item.label}
-                </Link>
-              </p>
-            );
-          })}
-        </GridWrap>
-        <GridWrap>
+      <Grid container>
+        <Grid item xs={8} sm={8} md={8} lg={8} xl={8}>
+          <div>
+            <div>Our Affiliations</div>
+            <div style={{ display: "flex", justifyContent: "space-around" }}>
+              <div>
+                <img src="/affiliations/FBR.png" alt="" width="100px" />
+              </div>
+              <div>
+                <img src="/affiliations/PATO.png" alt="" width="100px" />
+              </div>
+              <div>
+                <img src="/affiliations/secp.png" alt="" width="100px" />
+              </div>
+              <div>
+                <img
+                  src="/affiliations/gov-pakistan.png"
+                  alt=""
+                  width="100px"
+                />
+              </div>
+            </div>
+          </div>
+          <div>
+            <Grid container className={footerContainer}>
+              <GridWrap>
+                <h3>Location</h3>
+                {address.map((el, id) => (
+                  <p key={id}>{el}</p>
+                ))}
+              </GridWrap>
+              <GridWrap>
+                <h3>Social Media</h3>
+                {Object.keys(links).map((item: any, id: number) => {
+                  const url = links[item];
+                  return (
+                    <Link href={url} target="_blank" className={link} key={id}>
+                      {icons[item]}
+                    </Link>
+                  );
+                })}
+              </GridWrap>
+              <GridWrap>
+                <h3>Links</h3>
+                {siteLinks.map((item: any, id: number) => {
+                  return (
+                    <p key={id}>
+                      <Link
+                        href={item.href}
+                        target="_blank"
+                        className={link}
+                        underline="none"
+                      >
+                        {item.label}
+                      </Link>
+                    </p>
+                  );
+                })}
+              </GridWrap>
+            </Grid>
+          </div>
+        </Grid>
+        <Grid item xs={4} sm={4} md={4} lg={4} xl={4}>
           <h3>Contact</h3>
           <a href={waLink} target="_black">
-            <img src="/others/qr-code.png" alt="" width="60px" height="60px" />
+            <img src={qrUrl} alt={qrAlt} width="60px" height="60px" />
           </a>
           <p style={{ textDecoration: "none" }}>{email}</p>
-        </GridWrap>
+        </Grid>
       </Grid>
+
       <Typography align="center" className={copyright}>
         @Copyright! All Rights Reserved 2021
       </Typography>
